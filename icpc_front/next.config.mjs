be8b34b11cc22c0a1265c6f83f4f5e7ca5b478a8
AswinJ1 <aswinjhome@gmail.com>
2026-03-10 17:21:51 +0530

    feat:update the details

--- icpc_front/next.config.mjs
+++ icpc_front/next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   output: "standalone",
   images: {
     domains: ['picsum.photos'],  // Place holder images used in gallery page
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   async redirects() {
     return [
