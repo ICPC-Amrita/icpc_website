db8f548ca22274a3f9238e5c4f06a57f3a4f301c
AswinJ1 <aswinjhome@gmail.com>
2026-03-11 12:23:51 +0530

    feat: update the website

--- icpc_front/components/about/perks_2.jsx
+++ icpc_front/components/about/perks_2.jsx
@@ -66,7 +66,7 @@ export default function ExclusivePerks2() {
           <div className="flex gap-[2vw] items-center">
             <div className="w-[15vw] h-[15vw] max-md:w-[30vw] max-md:h-[30vw] rounded-[1vw] overflow-hidden flex-shrink-0">
               <Image
-                src="/ICPC_Photos/ICPC_25/Day-1/first.JPG"
+                src="/ICPC_Photos/ICPC_25/Day-1/first.jpg"
                 width={500}
                 height={500}
                 alt="Certificates"
